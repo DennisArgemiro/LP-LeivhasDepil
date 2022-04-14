f1824845ef85200bcbbfa230159171a0db2a78d6
DennisArgemiro <dennisarg2011@gmail.com>
2022-04-14 05:49:06 -0300

    Menu lateral adicionado no modo mobile

---
+++ script.js
@@ -0,0 +1,10 @@
+const hidden = document.querrySelector('.nav_img');
+
+
+hidden.addEventListener('click', function(){
+    const menu = document.querySelector('.hidden_menu');
+    menu.classList.toggle("active");
+})
+
+
+ 
